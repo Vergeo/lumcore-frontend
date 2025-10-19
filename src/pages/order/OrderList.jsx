0773fd0e68603ed8929b08c079b393988da0602f
Vergeo <vergeovalenting@gmail.com>
2025-10-19 11:43:39 +0700

    add date option to orderlist

--- src/pages/order/OrderList.jsx
+++ src/pages/order/OrderList.jsx
@@ -4,21 +4,28 @@ import Navbar from "../../components/Navbar";
 import useAxiosPrivate from "../../hooks/useAxiosPrivate";
 import { style } from "../../styles/style";
 import Order from "./Order";
+import useAuth from "../../hooks/useAuth";
 
 const OrderList = () => {
 	const navigate = useNavigate();
 	const axiosPrivate = useAxiosPrivate();
+	const { auth } = useAuth();
+	const [manager, setManager] = useState(false);
 
 	const [activeOrders, setActiveOrders] = useState([]);
 	const [finishedOrders, setFinishedOrders] = useState([]);
 	const [isLoading, setIsLoading] = useState(true);
+	const [date, setDate] = useState(new Date().toISOString().split("T")[0]);
 
 	const fetchOrders = async () => {
 		try {
 			setIsLoading(true);
 			const res = await axiosPrivate.get("sales");
 
-			const today = new Date();
+			const tempActiveOrders = [];
+			const tempFinishedOrders = [];
+
+			const today = new Date(date);
 			today.setHours(0, 0, 0, 0);
 
 			res.data.forEach((order) => {
@@ -26,13 +33,17 @@ const OrderList = () => {
 				orderDate.setHours(0, 0, 0, 0);
 
 				if (today.getTime() === orderDate.getTime()) {
+					console.log("TEST");
 					if (order.status === "active") {
-						setActiveOrders((prev) => [...prev, order]);
+						tempActiveOrders.push(order);
 					} else {
-						setFinishedOrders((prev) => [...prev, order]);
+						tempFinishedOrders.push(order);
 					}
 				}
 			});
+
+			setActiveOrders(tempActiveOrders);
+			setFinishedOrders(tempFinishedOrders);
 			setIsLoading(false);
 		} catch (error) {
 			setIsLoading(false);
@@ -43,14 +54,33 @@ const OrderList = () => {
 	};
 
 	useEffect(() => {
+		setManager(auth?.roles?.includes("Manager"));
 		fetchOrders();
 	}, []);
+
+	useEffect(() => {
+		fetchOrders();
+	}, [date]);
 
 	return (
 		<div className="w-screen min-h-screen bg-(--bg-dark) flex">
 			<Navbar />
 			<div className="w-full flex flex-col gap-6 p-6">
-				<h1 className={style.h1}>Daftar Pesanan Aktif</h1>
+				<div className="flex justify-between">
+					<h1 className={style.h1}>Daftar Pesanan Aktif</h1>
+					{manager && (
+						<input
+							type="date"
+							name=""
+							id=""
+							onChange={(e) => {
+								setDate(e.target.value);
+							}}
+							value={date}
+							className="bg-(--bg-light) hover:bg-(--accent-light) transition-all ease-in p-2 w-fit rounded-sm cursor-pointer shadow-md"
+						/>
+					)}
+				</div>
 				{isLoading ? (
 					<div className="flex justify-start items-center">
 						<i className="fa-solid fa-cog fa-spin mr-2"></i>
