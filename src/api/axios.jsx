8fe66246cf1d65246998a12c5d42be3ffa9fa285
Vergeo <vergeovalenting@gmail.com>
2026-05-07 10:30:03 +0700

    Move api to digital ocean

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 // const BASE_URL = "http://localhost:3500";
-const BASE_URL = "https://lumcore-api.onrender.com/";
+// const BASE_URL = "https://lumcore-api.onrender.com/";
+const BASE_URL = "https://lumcore-lhboi.ondigitalocean.app/";
 
 export default axios.create({
 	baseURL: BASE_URL,
